032c01207a836194e013224bd912059aeb3beeb8
Ankit Pati <contact@ankitpati.in>
2019-08-19 08:27:23 +0530

    upgrade install.pl

--- install.pl
+++ install.pl
@@ -1,31 +1,32 @@
-#! /usr/bin/env perl
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
+use autodie;
 
-my $installpath = shift;
-my $bindir = shift;
-my $mandir = shift;
-my $sysconfdir = shift;
+use File::Path qw(make_path);
+
+my ($installpath, $bindir, $mandir, $sysconfdir) = @ARGV;
 
 print "Installing yasql to $bindir/yasql\n";
-system("$installpath -m 755 yasql $bindir/yasql");
+system $installpath, qw(-m 755 yasql), "$bindir/yasql";
 
-unless(-e "$sysconfdir/yasql.conf") {
-  print "Installing yasql.conf to $sysconfdir/yasql.conf\n";
-  system("$installpath -m 644 yasql.conf $sysconfdir/yasql.conf");
-} else {
-  print "$sysconfdir/yasql.conf exists, skipping config file installation.\n";
-  print "Please read the documentation for info on any new ".
-        "configuration directives\n";
+unless (-e "$sysconfdir/yasql.conf") {
+    print "Installing yasql.conf to $sysconfdir/yasql.conf\n";
+    system $installpath, qw(-m 644 yasql.conf), "$sysconfdir/yasql.conf";
+}
+else {
+    print "$sysconfdir/yasql.conf exists, skipping config file installation.\n";
+    print "Read documentation for info on any new configuration directives.\n";
 }
 
-if( -f "yasql.1" ) {
-  print "Installing yasql.1 man page to $mandir/man1/yasql.1\n";
-  system("mkdir -p $mandir/man1");
-  system("$installpath -m 644 yasql.1 $mandir/man1/yasql.1");
-} else {
-  warn "yasql.1 not found, use perldoc $bindir/yasql to read the docs"
+if (-f "yasql.1") {
+    print "Installing yasql.1 man page to $mandir/man1/yasql.1\n";
+    make_path "$mandir/man1";
+    system $installpath, qw(-m 644 yasql.1), "$mandir/man1/yasql.1";
+}
+else {
+    warn "yasql.1 not found, use perldoc $bindir/yasql to read the docs";
 }
 
-print "Installation successfull\n";
-
+print "Installation successful.\n";
